1201f38d6e13c60d385099a5c15ad664df4e20fe
Safiy Cham <csafiy@yahoo.com>
2018-11-17 15:12:02 -0800

    Checkbox for public/private search

--- lantern/src/search/Search.js
+++ lantern/src/search/Search.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
 
-
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { checked: false };
+  }
+
+  handleCheckClick = () => {
+    const privateBox = document.querySelector(".private-box");
+
+    this.setState({ checked: !this.state.checked });
+
+    if (this.state.checked === true) {
+
+      privateBox.style.display = "inline";
+    } else {
+      privateBox.style.display = "none";
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <form>
           <label>
-            Enter comma separated behavior terms: 
-              <br/>
-              <input type="text" name="search" />
+            Enter comma separated behavior terms:
+              <br />
+            <input type="text" name="search" />
           </label>
-          <br/> 
+          <div>
+            <input type="checkbox" checked={this.state.checked} onChange={this.handleCheckClick} />
+            <div>Public</div>
+            <div className="private-box">
+              School Name: <input type="text" /><br />
+              School Zip: <input type="text" /><br />
+            </div>
+          </div>
+          <br />
           <input type="submit" value="Search" />
-          
-          <input
-            name="Public"
-            type="checkbox"
-          />Public
         </form>
       </div>
     );
