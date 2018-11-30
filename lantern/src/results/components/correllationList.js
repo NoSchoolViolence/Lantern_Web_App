571396ca476beaf5535237b61aa88d04dba7a13a
dahby <dstoll243@gmail.com>
2018-11-29 19:06:53 -0800

    modal now populating with correct information

--- lantern/src/results/components/correllationList.js
+++ lantern/src/results/components/correllationList.js
@@ -2,31 +2,22 @@ import React, { Component } from "react";
 import Modal from './modal';
 
 class CorrellationList extends Component {
-  // constructor() {
+  constructor() {
+    super();
+    this.state = { 
+      show: false,
+      activeIndex: null
+    };
+  }
 
-  //   super();
-
-    
-  // }
-
-  state = { show: false };
-  showModal = () => {
-    this.setState({ show: true });
-  };
+  showModal = (i) => {
+    this.setState({ activeIndex: i })
+  }
 
   hideModal = () => {
     this.setState({ show: false });
+    this.setState({ activeIndex: null })
   }
-
-  //   this.state = { editing: false };
-
-  //   this.toggleModal = this.toggleModal.bind(this);
-  // }
-
-  // toggleModal() {
-  //   let editing = !this.state.editing;
-  //   this.setState({ editing })
-  // }
 
   render() {
     let data = [
@@ -34,96 +25,66 @@ class CorrellationList extends Component {
         id: "1",
         name: "bullying",
         percentage: 10,
-        description: "bully Ipsum",
+        description: "bullying",
         link: 'placeholder'
       },
       {
         id: "2",
         name: "weapons use",
         percentage: 20,
-        description: "WU Ipsum",
+        description: "weapons use",
         link: 'placeholder'
       },
       {
         id: "3",
         name: "sexual harrasment",
         percentage: 30,
-        description: "More SH Ipsum",
+        description: "sexual harrasment",
+        link: 'placeholder'
+      }, {
+        id: "10",
+        name: "sucidal ideation",
+        percentage: 85,
+        description: "suicidal ideation",
         link: 'placeholder'
       }, {
         id: "4",
         name: "self-harm",
         percentage: 40,
-        description: "SH Ipsum",
-        link: 'placeholder'
-      }, {
-      //   id: "5",
-      //   name: "fighting",
-      //   percentage: 50,
-      //   description: "Ipsum",
-      //   link: 'placeholder'
-      // }, {
-      //   id: "6",
-      //   name: "drug use",
-      //   percentage: 60,
-      //   description: "Ipsum",
-      //   link: 'placeholder'
-      // }, {
-      //   id: "7",
-      //   name: "gang activities",
-      //   percentage: 70,
-      //   description: "Ipsum",
-      //   link: 'placeholder'
-      // }, {
-      //   id: "8",
-      //   name: "verbal abuse",
-      //   percentage: 80,
-      //   description: "Ipsum",
-      //   link: 'placeholder'
-      // }, {
-      //   id: "9",
-      //   name: "sexual violence",
-      //   percentage: 90,
-      //   description: "Ipsum",
-      //   link: 'placeholder'
-      // }, {
-        id: "10",
-        name: "sucidal ideation",
-        percentage: 85,
-        description: "SI ipsum",
+        description: "selfharm",
         link: 'placeholder'
       }
     ];
-
     return (
       <div className="CorrellationList">
+        {
+          data.map((item, i) => {
 
-          {
-            data.map((item, i) => {
+            const pStyle = {
+              height: 50,
+              width: `${item.percentage}%`
+            }
 
-              const pStyle = {
-                height: 50,
-                width: `${item.percentage}%`
-              }
-
-              return (
-                <div key={i}>
-                <Modal show={this.state.show} handleClose={this.hideModal}>
-                  <p>Description: {item.description}</p>
+            return (
+              <div key={i} className="CorrellationContainer">
+                {/* {console.log(i)} */}
+                <Modal show={this.state.activeIndex === i ? true : false} handleClose={this.hideModal}>
+                  <p>Description: { item.description }</p>
                   <a href={item.link}>More Information</a>
                 </Modal>
-                  <div onClick={this.showModal} className="CorrellationItem">
-                    <h3>Violence Type: {item.name}</h3>
-                    <div className='percentContainer'>
-                      <div className="ItemPercent" style={pStyle}></div>
+
+                <div onClick={() => this.showModal(i)} className='CorrellationItem' >
+                  <h3>Violence Type: {item.name}</h3>
+                  <div className='percentContainer'>
+                    <div className="ItemPercent" style={pStyle}>
                     </div>
-                    {/* <p>Description: {item.description}</p>
-                    <a href={item.link}>More Information</a> */}
                   </div>
                 </div>
-              )
-            })
-          }
+
+              </div>
+            )
+          })
+        }
       </div>
     );
   }
--- lantern/src/results/components/modal.js
+++ lantern/src/results/components/modal.js
@@ -1,34 +1,17 @@
 import React from 'react';
 
-const Modal = ({ handleClose, show, children }) => {
-  const showHideClassName = show ? "modal display-block" : 'modal display-none';
+const Modal = (props) => {
+  const showHideClassName = props.show ? "modal display-block" : 'modal display-none';
 
   return (
     <div className={showHideClassName}>
       <div className="modal-main">
-        {children}
-        <button onClick={handleClose}>Close Me</button>
+        {/* {console.log(props.children)} */}
+        {props.children}
+        <button onClick={props.handleClose}>Close Me</button>
       </div>
     </div>
   )
 }
-
-// class Modal extends React.Component {
-
-//   render() {
-
-//     return (
-
-      {/* <div className="overlay">
-        <div className="modal">
-          {this.props.children}
-          <button onClick={this.props.close}>Close Me</button>
-        </div>
-      </div> */}
-//     )
-
-//   }
-
-// }
 
 export default Modal;
