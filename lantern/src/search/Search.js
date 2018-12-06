0081dcb4de148d41a2b0c6612206b584a0a9ae22
neelip <neelip@uw.edu>
2018-12-05 18:16:43 -0800

    recent changes

--- lantern/src/search/Search.js
+++ lantern/src/search/Search.js
@@ -1,37 +1,152 @@
 import React, { Component } from 'react';
-import ROUTES from '../routes';
+
 
 class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state={todos:[]};
+  constructor(props){
+    super(props)
+    this.state = {
+      name: 'Enter the behaviors you notice',
+      tasks: []
+    }
+  
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.addTask = this.addTask.bind(this);
+    this.removeTask = this.removeTask.bind(this);
+    this.task = this.task.bind(this);
   }
+
+  handleSubmit(event){
+    event.preventDefault()
+    //event.target.name(event)
+  }
+  task(event) {
+    this.setState({task:event.target.value})
+  }
+  addTask(event) {
+    if (!this.state.task) return
+    const tasks = this.state.tasks
+    tasks.push(this.state.task)
+    this.setState({tasks:tasks, task:''})
+  }
+  removeTask(index, event) {
+    const tasks = this.state.tasks
+    tasks.splice(index, 1)    
+    this.setState({tasks})
+  }
+  render(){
+    const tasks = (this.state.tasks).map((task,index)=>(
+      <li>
+        {task} <button name="removeTask" onClick={event=>this.removeTask(index,event)}>x</button>
+      </li>
+    ))
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <div>
+          <ol>
+            {tasks}
+            {
+              this.state.task &&
+              <li>{this.state.task}</li>
+            }
+          </ol>
+          <div>
+            <form  onSubmit={this.handleSubmit}>
+              <input value={this.state.task} onChange={this.task}/>
+              <button type="submit" onClick={this.addTask}>Add</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+ /*  constructor(props) {
+    super(props);
+    this.state={
+      todos:[],
+      count: 0,
+    };
+
+    this.removeString = this.removeString.bind(this);
+    this.save = this.save.bind(this);
+    this.handleClickIndex = this.handleClickIndex.bind(this);
+  }
+
+ 
   
   save(e) {
+  
     if(e.keyCode === 13){
+    var count = this.state.count + 1;
+    this.setState({count:count});
+    var item = {value: this.newText.value}
+
     var todos = [...this.state.todos];
-    todos.push(this.newText.value);
-    this.setState({todos});
+    todos.push(item);
+    this.setState({todos: todos});
+    
+    // Resets the textbox after "enter"
+    this.newText.value ="";
     }
+
+    
+    
+  }
+
+  removeString(key) {
+    //var todos = [...this.state.todos];
+
+    
+    
+    //todos.splice( todos.indexOf(todo), 1 );
+    /* var groupKey =  todo.groupKey;
+    if (todos.hasOwnProperty(groupKey)){
+      todos.pop();
+    } 
+    
+   this.setState({todos:this.state.todos.filter((key, _) => key !== key)});
+  }
+
+ /*  removeString(key, value){
+    
+   
+  }  
+
+
+
+  submit(e){
+
+
+
+  }
+
+  handleClickIndex(index, event){
+    eval(this[event.target.name]).bind(this)(index, event)
+  }
+  
+  removeTask(index, event) {
+    const tasks = this.state.todos
+    tasks.splice(index, 1)    
+    this.setState({todos:tasks})
   }
 
   render(){
       return(
           <div className="list">
             <h1> TO-DO List</h1>
-            <input type="text" ref={(ip) => {this.newText = ip}} onKeyDown={this.save.bind(this)}/>
-            <button onClick={this.save.bind(this)} className="btn btn-primary glyphicon glyphicon-floppy-saved">Save
+            <input type="text" ref={(ip) => {this.newText = ip}} onKeyDown={this.save}/>
+            <button  name="removeTask" onClick={this.submit.bind(this)} className="btn btn-primary glyphicon glyphicon-floppy-saved">Save
             </button>
             <ul>
-              {this.state.todos.map(function(todo) {
-                    return <li>{todo}</li>
+              {this.state.todos.map((value, index) => {
+                    return <button key = {index} onClick={(e) => this.removeTask.bind(this, index)} className="btn btn-primary glyphicon glyphicon-floppy-saved">{value}</button>
                })}
               
-            </ul>
+            </ul> 
           </div>
       )
   }
-
+ */
 /*   getInitialState(){
     return {
       todos: []
@@ -74,7 +189,7 @@ class Search extends Component {
               <br/>
               <input type="text" value={this.state.value} onKeyDown={this.keyPress} onChange={this.handleChange} name="search" fullWidth={true}/>
           </label>
-          <br />
+          <br/> 
           <input type="submit" value="Search" />
           
           
