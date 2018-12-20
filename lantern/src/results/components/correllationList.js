ebfd2eeed8afc5a01881c7761cd2b921cbf12895
dahby <dstoll243@gmail.com>
2018-12-20 13:35:03 -0800

    test data now populating as expected, will work to redo dataset to work for all search terms

--- lantern/src/results/components/correllationList.js
+++ lantern/src/results/components/correllationList.js
@@ -9,7 +9,7 @@ class CorrellationList extends Component {
       showDetails: false,
       activeIndex: null,
       data: this.props.dataset,
-      terms: this.props.terms
+      terms: this.props.terms.join(', '),
     };
   }
 
@@ -22,53 +22,55 @@ class CorrellationList extends Component {
     this.setState({ activeIndex: null })
   }
 
-
+  // findList = () => {
+  //   let termString = this.state.terms.join(', ');
+  //   // console.log(termString);
+  //   this.setState({ searchString: termString });
+  // }
 // Displays a div 
 // when the 2nd search term equals the 1st item.name from listOfViolences 
 // in the sample dataset in data.js
 
 
   render() {
-    
-    const terms = (this.state.data).map((term,index)=>
-      term.listOfViolence.map((item, i) => {
-        if (item.name == this.state.terms[1]) {
-        const pStyle = {
-            height: 50,
-            width: `${item.percentage}%`
-            
-          }
-        return (
-          <div key={i} className="CorrellationContainer">
-          <div className='CorrellationItem' >
+    let violenceList = null;
+    for (let i = 0; i < this.state.data.length; i++) {
+      if (this.state.data[i].name === this.state.terms) {
+        violenceList = this.state.data[i].listOfViolence
+      }
+    }
+
+    return (
+      <div className="CorrellationList">
+        {
+          violenceList.map((item, i) => {
+
+            const pStyle = {
+                height: 50,
+                width: `${item.percentage}%`
+            }
+
+            return (
+              <div key={i} className="CorrellationContainer">
+              <Modal show={this.state.activeIndex === i ? true : false} handleClose={this.hideModal}>
+                  <p><strong>Description:</strong> { item.description }</p>
+                  <a href={item.link}>More Information</a>
+                </Modal>
+                
+                <div onClick={() => this.showModal(i)} className='CorrellationItem' >
                   <h3>Violence Type: {item.name}</h3>
                   <div className='percentContainer'>
                     <div className="ItemPercent" style={ pStyle }>
                     </div>
                   </div>
                 </div>
-          </div>
-
-        )
-        
-        
-        
-        
+              </div>
+            )
+            })
         }
-      }
-        
-      
-      
-    ))
-
-
-    
-    return (
-      <div>{terms}</div> 
-    )
-
-    
-  }
+        </div>
+    );
+}
 
 }
 
